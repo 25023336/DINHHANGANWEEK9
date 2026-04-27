82b44259f8ec1e35c64137bc287cd23c3e5d0c14
Dinh Ha Ngan <25023336@vnu.edu.vn>
2026-04-27 09:48:02 +0700

    fix: make PathService cross-platform with Path.of(...).resolve(...)

--- src/main/java/org/example/PathService.java
+++ src/main/java/org/example/PathService.java
@@ -5,6 +5,6 @@ import java.nio.file.Path;
 public class PathService {
 
     public static Path buildPath(String folder, String fileName) {
-        return Path.of(folder + "\\" + fileName);
+        return Path.of(folder).resolve(fileName);
     }
 }
